d26b08d0aa23497a37d8368c38e4bb2944662a8d
Sven REMOND <62282366+SvenREMOND@users.noreply.github.com>
2023-10-09 09:24:22 +0000

    front form compte mis en place

--- public/js/forms.js
+++ public/js/forms.js
@@ -9,3 +9,46 @@ labels.forEach((label) => {
 		e.target.parentElement.classList.add("active");
 	});
 });
+
+class magicFocus {
+	reset;
+	focus;
+
+	constructor(form) {
+		this.focus = document.createElement("div");
+		this.focus.classList.add("magic-focus");
+		form.append(this.focus);
+
+		form.querySelectorAll("input, textarea, select").forEach((input) => {
+			input.addEventListener("focus", (e) => {
+				this.show(e.target);
+			});
+			input.addEventListener("blur", (e) => {
+				this.hide(e.target);
+			});
+		});
+	}
+
+	show(el) {
+		clearTimeout(this.reset);
+
+		if (el.type == "radio") {
+			el = document.querySelector(`[for=${el.id}]`);
+		}
+
+		this.focus.style.top = (el.offsetTop || 0) + "px";
+		this.focus.style.left = (el.offsetLeft || 0) + "px";
+		this.focus.style.width = (el.offsetWidth || 0) + "px";
+		this.focus.style.height = (el.offsetHeight || 0) + "px";
+	}
+
+	hide(el) {
+		this.reset = setTimeout(() => {
+			this.focus.removeAttribute("style");
+		});
+	}
+}
+
+document.querySelectorAll("form").forEach((form) => {
+	new magicFocus(form);
+});
